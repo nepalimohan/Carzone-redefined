b6f18b1359596401aa235a6e78d57f2a7202d543
Mohan Nepali <nepalimohan@outlook.com>
2022-03-07 19:44:49 +0545

    About, contact, services added

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from pages.views import home
+from pages.views import home, services, contact, about
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('services', services, name='services'),
 ]
